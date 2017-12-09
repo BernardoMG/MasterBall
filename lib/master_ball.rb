baf5c53bf8453dc86d9aa1320a4f0b8cd58bf11d
Bernardo Graça <bernardo.marquesg@gmail.com>
2017-12-09 11:49:16 +0000

    added game functional code

--- lib/master_ball.rb
+++ lib/master_ball.rb
@@ -7,22 +7,42 @@ class MasterBall
     @position_x = 0
     @position_y = 0
     @pokemon_map = Set.new []
+    @pokemon_map << ("#{@position_x};#{@position_y}")
     input = gets.chomp
-    inputValidation(input)
+    input_validation(input)
   end
 
   # private methods are grouped near the end
   private
   
   # comment
-  def inputValidation(input) 
+  def input_validation(input) 
     if input.nil? || input.empty?
       puts @pokemon_map.size
     else
-      puts @position_x
-      puts @position_y     
+      input.split('').each do |c| 
+        update_map(c)
+      end
       puts @pokemon_map.size
-      # @pokemon_map.add(input)
+    end
+  end
+
+  # comment
+  def update_map(move)
+    case move
+    when "N"
+      @position_y += 1
+      @pokemon_map << ("#{@position_x};#{@position_y}")
+    when "S"
+      @position_y -= 1
+      @pokemon_map << ("#{@position_x};#{@position_y}")
+    when "E"
+      @position_x += 1
+      @pokemon_map << ("#{@position_x};#{@position_y}")
+    when "O"
+      @position_x -= 1
+      @pokemon_map << ("#{@position_x};#{@position_y}")   
+    else  
     end
   end
 end
