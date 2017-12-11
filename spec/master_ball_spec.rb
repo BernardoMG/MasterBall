0e69728b373b6f723eb36941f7e04e4247bf07a5
Bernardo Graça <bernardo.marquesg@gmail.com>
2017-12-11 22:26:09 +0000

    added new tests

--- spec/master_ball_spec.rb
+++ spec/master_ball_spec.rb
@@ -7,14 +7,47 @@ describe MasterBall do
 
   subject { @master_ball }
 
-  describe 'catching some pokemons' do
+  describe 'catching pokemons' do
 
-    it 'should catch only two pokemons' do
+    it 'should catch two pokemons, one move' do
       moves = "N"
       expected_pokemons = "2\n"
       expect {
         subject.init_map(moves)
       }.to output(expected_pokemons).to_stdout
+    end
+
+    it 'should catch four pokemons, four moves' do
+      moves = "NESO"
+      expected_pokemons = "4\n"
+      expect {
+        subject.init_map(moves)
+      }.to output(expected_pokemons).to_stdout
+    end
+
+    it 'should catch two pokemons, ten moves' do
+      moves = "NSNSNSNSNS"
+      expected_pokemons = "2\n"
+      expect {
+        subject.init_map(moves)
+      }.to output(expected_pokemons).to_stdout
+    end
+  end
+
+  describe 'tricky moves' do
+    
+    it 'should catch one pokemon, empty move' do
+      expected_pokemons = "1\n"
+      expect {
+        subject.init_map("")
+      }.to output(expected_pokemons).to_stdout 
+    end
+
+    it 'should catch one pokemon, null move' do
+      expected_pokemons = "1\n"
+      expect {
+        subject.init_map(nil)
+    }.to output(expected_pokemons).to_stdout
     end
   end
 end
