f906d38bf9978ace6c80c90a1f5ef1c0763ccbc1
Bernardo Graça <bernardo.marquesg@gmail.com>
2017-12-12 15:11:43 +0000

    added comments to source code

--- lib/master_ball.rb
+++ lib/master_ball.rb
@@ -2,13 +2,13 @@ require 'set'
 
 class MasterBall
 
-  # comment
+  # input read
   def run
     input = gets.chomp
     init_map(input)
   end
 
-  # comment
+  # pokemon map setup and catch the first one at the initial position
   def init_map(input)
     @position_x = 0
     @position_y = 0
@@ -20,7 +20,7 @@ class MasterBall
   # private methods are grouped near the end
   private
   
-  # comment
+  # input validation, namely for empty or null moves
   def input_validation(input) 
     if input.nil? || input.empty?
       puts @pokemon_map.size
@@ -32,7 +32,7 @@ class MasterBall
     end
   end
 
-  # comment
+  # moves validation, map update and add more pokemons to the collection
   def update_map(move)
     case move
     when "N"
